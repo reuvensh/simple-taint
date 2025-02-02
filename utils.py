4b177ed68b9c15b55bedfcb75710eda1fad43f95
reuvensh <reuvenshirazi@gmail.com>
2025-02-02 10:11:41 +0200

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,14 @@
+import time
+from pathlib import Path
+
+
+def file_write(item_id: str, file_content: str) -> bool:
+    # writes to file - danger of directory traversal
+    if Path("items", f"{item_id}").exists():
+        with open(f"{item_id}", "w") as f:  # sink
+            time.sleep(1)
+            f.write(file_content)
+        return True
+    else:
+        time.sleep(0.1)
+        return False
